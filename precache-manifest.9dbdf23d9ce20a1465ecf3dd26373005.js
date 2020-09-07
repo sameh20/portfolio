139177dc81d0bc7c30b102b731cac9072693f34c
sameh20 <samehaboelmagd49@gemail.com>
2020-09-07 01:46:06 -0700

    Updates

---
+++ precache-manifest.9dbdf23d9ce20a1465ecf3dd26373005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a847ce7f9d0332c94d35e4d743516936",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "68e2ffa21008cf2933a5",
+    "url": "/portfolio/static/css/main.25b39518.chunk.css"
+  },
+  {
+    "revision": "4e53094bb6953dda340e",
+    "url": "/portfolio/static/js/2.106e3d92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.106e3d92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68e2ffa21008cf2933a5",
+    "url": "/portfolio/static/js/main.1993dcc1.chunk.js"
+  },
+  {
+    "revision": "3f1f8e742c99aba094bb",
+    "url": "/portfolio/static/js/runtime-main.4e966a21.js"
+  }
+]);
